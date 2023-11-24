e9b99b9a53ccab55e42b2d15ca851fabcfbc214e
Edgar Gaxiola <116056213EddieGaxiola@users.noreply.github.com>
2023-11-24 15:15:21 -0800

    Create rough draft of game function that loops 5 rounds of singleRound function and tracks score

--- script.js
+++ script.js
@@ -21,34 +21,50 @@ function singleRound(playerSelection, computerSelection) {
     
     //compare player input with comp input and *return* message saying "you lose" or "you win" and state what beats what
     if(playerSelection == computerSelection) {
+        roundScore = roundScore;
         return "It's a Draw";
     }
     else if( (playerSelection == "Rock") && (computerSelection == "Paper")) {
+        roundScore = roundScore;
         return "You Lose! Paper beats Rock";
     }
     else if( (playerSelection == "Rock") && (computerSelection == "Scissors")) {
+        roundScore = "Win";
         return "You Win! Rock beats Scissors";
     }
     else if( (playerSelection == "Paper") && (computerSelection == "Rock") ) {
+        roundScore = "Win";
         return "You Win! Paper beats Rock";
     }
     else if( (playerSelection == "Paper") && (computerSelection == "Scissors") ) {
+        roundScore = "Lose";
         return "You Lose! Scissors beat Paper";
     }
     else if( (playerSelection == "Scissors") && (computerSelection == "Rock") ) {
+        roundScore = "Lose";
         return "You Lose! Rock beats Scissors";
     }
     else if( (playerSelection == "Scissors") && (computerSelection == "Paper") ) {
+        roundScore = "Win";
         return "You Win! Scissors beat Paper";
     }
 }
 
-//function that runs loop for 5 rounds of function singleRound()
+//function that runs loop for 5 rounds of singleRound()
 function game() {
     for(let i = 0; i < 5; i++) {
-        playerSelection = prompt("Pick Rock, or Paper, or Scissors: ", );
+        playerSelection = prompt("Pick Rock, or Paper, or Scissors: ");
         singleRound(playerSelection, computerSelection);
-        
+        if(roundScore = "Win") {
+            gameScore++;
+        }
+
+    }
+    if(gameScore >= 3) {
+        console.log(`You Won the Game. Score = ${gameScore} out of 5`);
+    }
+    else if(gameScore < 3) {
+        console.log(`You Lost the Game. Score = ${gameScore} out of 5`);
     }
 }
 
@@ -56,5 +72,7 @@ function game() {
 
 const playerSelection = "pAper";
 const computerSelection = getComputerChoice();
+let roundScore = "";
+let gameScore = 0;
 
 console.log(singleRound(playerSelection, computerSelection));
